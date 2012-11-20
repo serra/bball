3c1a01c7a31560dfd5ae1a8f455dfb158025ef11
Marijn van der Zee <marijn@serraict.com>
2012-11-20 10:39:37 +0100

    Export google-compliant csv file

--- dbl/Process2012-13.R
+++ dbl/Process2012-13.R
@@ -1,4 +1,4 @@
-# Create data files with advances stats for the 2012-2013
+# Create data files with advances stats for the 2012-2013 season
 
 source("./src/CalculateAdvancedGameStats.r")
 source("./src/download.r")
@@ -10,3 +10,13 @@ CreateAdvancedStatsFiles(fileName)
 # read to check results:
 regseasTeam <- read.csv2(sprintf("./output/heren_%s_regseas_advanced_team_stats.csv", season))
 regseasPlyr <- read.csv2(sprintf("./output/heren_%s_regseas_advanced_player_stats.csv", season))
+
+# suitable for google docs:
+googleTeamFile <- sprintf("./output/heren_%s_regseas_advanced_team_stats_google.csv", season)
+googlePlayerFile <- sprintf("./output/heren_%s_regseas_advanced_player_stats_google.csv", season)
+
+# write.csv complies to google's csv expectaions,
+# while write.csv2 complies to Excel in most Western European locales
+# see help(write.csv) for details
+write.csv(regseasTeam, googleTeamFile)
+write.csv(regseasPlyr, googlePlayerFile)
