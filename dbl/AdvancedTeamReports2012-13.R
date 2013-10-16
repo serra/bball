c19a1fbcbcbf1f9c4182399808bc397c915d05d2
Marijn van der Zee <marijn@serraict.com>
2013-10-16 11:17:03 +0200

    Archive some old scripts

--- dbl/AdvancedTeamReports2012-13.R
+++
@@ -1,8 +0,0 @@
-# Create pdf with advanced team stats
-source("./src/ReportTeamRatings.r")
-
-season <- "2012-2013" 
-
-regseasTeam <- read.csv2(sprintf("./output/heren_%s_regseas_advanced_team_stats.csv", season))
-
-PrintTeamRatings(regseasTeam, sprintf("./output/%s_regseas_advanced_teamRatings.pdf", season))
