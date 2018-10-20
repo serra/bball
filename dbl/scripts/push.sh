a481e6bb09898de4264bd8275dedbd3d9a646975
Marijn van der Zee <marijn@serraict.com>
2018-10-20 17:43:54 +0200

    Set my own username

--- dbl/scripts/push.sh
+++ dbl/scripts/push.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "marijn@serraict.com"
+  git config --global user.name "serra"
 }
 
 add_changes_to_master_branch() {
   git branch -f master HEAD #update master to point to the new <temp> branch
-  git log --oneline master -n 5  
+  git log --oneline master -n 5
 }
 
 upload_files() {
-  echo "remote add origin https://${GH_TOKEN}@github.com/serra/bball.git"
   git remote add origin https://serra:${GH_TOKEN}@github.com/serra/bball.git > /dev/null 2>&1
-  echo "pushing to origin ..."
   git push --quiet --set-upstream origin master
-  echo "done"
 }
 
 setup_git
