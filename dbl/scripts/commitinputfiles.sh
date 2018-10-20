f574f8faf57cc65d6fd6b9a42dcb5d2420c2ab5d
Marijn van der Zee <marijn@serraict.com>
2018-10-20 11:41:11 +0200

    Commit changes to input files if any

--- dbl/scripts/commitinputfiles.sh
+++ dbl/scripts/commitinputfiles.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 mydir=$(dirname "${BASH_SOURCE[0]}")
-echo $mydir
+inputdir = "$mydir/../input"
 git --version
 
 # if we have new input files, we should push them to github
-# git status dbl/input --porcelain
+git status dbl/input --porcelain
 
-# if [ -n "$(git status dbl/input --porcelain)" ]; then 
-#   GAMES=$(git diff -U0 ./dbl/input/*.csv | 
-#         grep -v '^@@' | 
-#         awk -F\; 'NR>4 {print $11 " " $9 " @ " $6 }' | 
-#         sort -r | uniq)
-#   MESSAGE=$"input files changed: $GAMES"
-#   git add dbl/input/**;
-#   git commit -m "$MESSAGE"
-# else 
-#   echo "no changes to input files";
-# fi
+if [ -n "$(git status $inputdir --porcelain)" ]; then 
+  GAMES=$(git diff -U0 $inputdir/*.csv | 
+        grep -v '^@@' | 
+        awk -F\; 'NR>4 {print $11 " " $9 " @ " $6 }' | 
+        sort -r | uniq)
+  MESSAGE=$"input files changed: $GAMES"
+  git add $inputdir/**;
+  git commit -m "$MESSAGE"
+else 
+  echo "no changes to input files";
+fi
