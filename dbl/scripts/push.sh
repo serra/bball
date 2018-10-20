ccd41b53a9919fbe2ebcdc7bed210020dbced2b7
Marijn van der Zee <marijn@serraict.com>
2018-10-20 17:24:41 +0200

    Move logging

--- dbl/scripts/push.sh
+++ dbl/scripts/push.sh
@@ -7,11 +7,11 @@ setup_git() {
 
 add_changes_to_master_branch() {
   git branch -f master HEAD #update master to point to the new <temp> branch
+  git log --oneline master -n 5  
 }
 
 upload_files() {
   echo "remote add origin https://${GH_TOKEN}@github.com/serra/bball.git"
-  git log --oneline master -n 5  
   git remote add origin https://${GH_TOKEN}@github.com/serra/bball.git > /dev/null 2>&1
   echo "pushing to origin ..."
   git push --quiet --set-upstream origin master
