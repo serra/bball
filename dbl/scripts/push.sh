8c6ae6178514c9d869ef722dec620978909d9a7f
Marijn van der Zee <marijn@serraict.com>
2018-10-20 15:52:07 +0200

    Push changes on input file to the master branch

--- dbl/scripts/push.sh
+++ dbl/scripts/push.sh
@@ -5,10 +5,17 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
+add_changes_to_master_branch() {
+  git checkout –b temp #makes a new branch from current detached HEAD
+  git branch –f master temp #update master to point to the new <temp> branch
+  git branch –d temp #delete the <temp> branch
+}
+
 upload_files() {
   git remote add origin https://${GH_TOKEN}@github.com/serra/bball.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master 
 }
 
 setup_git
+add_changes_to_master_branch
 upload_files
