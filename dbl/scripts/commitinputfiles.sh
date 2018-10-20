7c328e930b52b54c99bb60e827a376e7185ac4a1
Marijn van der Zee <marijn@serraict.com>
2018-10-20 10:44:28 +0200

    Call commit_input_files from makefile

--- dbl/scripts/commitinputfiles.sh
+++ dbl/scripts/commitinputfiles.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+echo "$(dirname $0)"
 git --version
+
+# if we have new input files, we should push them to github
+# git status dbl/input --porcelain
+
+# if [ -n "$(git status dbl/input --porcelain)" ]; then 
+#   GAMES=$(git diff -U0 ./dbl/input/*.csv | 
+#         grep -v '^@@' | 
+#         awk -F\; 'NR>4 {print $11 " " $9 " @ " $6 }' | 
+#         sort -r | uniq)
+#   MESSAGE=$"input files changed: $GAMES"
+#   git add dbl/input/**;
+#   git commit -m "$MESSAGE"
+# else 
+#   echo "no changes to input files";
+# fi
