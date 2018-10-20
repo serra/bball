053439226525d790559ff6f06e2e30979433a146
Marijn van der Zee <marijn@serraict.com>
2018-10-20 15:59:28 +0200

    Fix outline

--- dbl/scripts/push.sh
+++ dbl/scripts/push.sh
@@ -6,9 +6,9 @@ setup_git() {
 }
 
 add_changes_to_master_branch() {
-  git checkout –b temp #makes a new branch from current detached HEAD
+  git checkout –b temp      #makes a new branch from current detached HEAD
   git branch –f master temp #update master to point to the new <temp> branch
-  git branch –d temp #delete the <temp> branch
+  git branch –d temp        #delete the <temp> branch
 }
 
 upload_files() {
