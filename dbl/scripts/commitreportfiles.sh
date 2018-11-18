45ef3a74000bcedf74215ae8f99e3e05d2b17af5
Marijn van der Zee <marijn@serraict.com>
2018-11-18 15:01:18 +0100

    Point to the correct directory
    There was a dot in line 8 that should not be there

--- dbl/scripts/commitreportfiles.sh
+++ dbl/scripts/commitreportfiles.sh
@@ -5,8 +5,8 @@ mydir=$(dirname "${BASH_SOURCE[0]}")
 reportdir="$mydir/../../site/pages/reports"
 
 if [ -n "$(git status $reportdir --porcelain)" ]; then 
-  git add ".$reportdir/**";
-  git commit -m "Updated report files"
+  git add "$reportdir/**";
+  git commit -m "Updated report files";
 else 
   echo "no changes to report files";
 fi
