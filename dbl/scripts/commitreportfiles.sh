bb8887f4a422460bc0501439b7fb65011b13b1eb
Marijn van der Zee <marijn@serraict.com>
2018-11-18 15:00:29 +0100

    Cleanup commit report files script

--- dbl/scripts/commitreportfiles.sh
+++ dbl/scripts/commitreportfiles.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-echo "Commit report files if changed"
+set -ev
+echo "Commit report files if changed ... "
 mydir=$(dirname "${BASH_SOURCE[0]}")
 reportdir="$mydir/../../site/pages/reports"
-
-echo "reportdir: $reportdir"
-git status
-ls -la $reportdir
-git status $reportdir
-git status $reportdir --porcelain
 
 if [ -n "$(git status $reportdir --porcelain)" ]; then 
   git add ".$reportdir/**";
