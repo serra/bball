8729c639f173ffae4b5753abbf9ade1de9094b19
Marijn van der Zee <marijn@serraict.com>
2018-11-20 16:16:29 +0100

    Add files from finished seasons

--- dbl/scripts/buildsite.sh
+++ dbl/scripts/buildsite.sh
@@ -7,11 +7,12 @@ sitedir="$mydir/../../site/"
 cd $sitedir
 tinker --build
 
-#copy charts
+#copy new output to local stats:
+cp -R -f -a ../dbl/output/* ./stats
+#copy stats and charts to target site
 cp -R -f -a ./charts ./blog/html
-#copy stats
-statsdir="./blog/html/stats"
-mkdir -p $statsdir && cp -R -f -a ../dbl/output/* $statsdir
+cp -R -f -a ./stats ./blog/html
+
 # add .nojekyll file to root of site
 touch ./blog/html/.nojekyll
 
